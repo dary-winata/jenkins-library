c7c629ee1b38f3edf58623083078647642ccfee5
Dary Winata <dary.winata15@gmail.com>
2021-09-17 15:45:55 +0700

    testing

--- vars/php.groovy
+++ vars/php.groovy
@@ -29,7 +29,7 @@ def call(Map param) {
             }
             stage('Run app') {
                 steps {
-                    'sh docker run -p "${param.port}" "${param.name}"'
+                    'sh docker run -p 8181:8080 "${param.name}"'
                 }
             }
         }
